221915a8c78faf8d86057ec5b70b51c62077db55
George <georgebradest@gmail.com>
2022-07-08 14:39:14 +0100

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# Robot Game
+
+# create player
+name = input(str("What is your name? "))
+if name.isnumeric():
+    print("Name Cant Be A Number")
+else:
+    print(f"Hello {name} welcome To Killer Robots!")
